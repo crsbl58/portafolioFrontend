d13dc497ac17a7aae03ff0bfdc12b74749688b2c
z.edd36 <z.edd@36gmail.com>
2022-06-26 22:15:24 -0500

    style: Sea color

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,7 +2,7 @@ const variablesconstcalls = () => ({
   stateDayNight: {
     sea: {
       backgroundColorDay00: { background: 'linear-gradient(180deg, rgba(100,175,203,1) 16%, rgba(83,144,196,1) 47%, rgba(46,95,120,1) 92%)' },
-      backgroundColorNight00: { background: 'linear-gradient(180deg, rgb(59, 158, 175) 16%, rgb(66, 110, 148) 47%, rgb(30, 60, 75) 92%)' },
+      backgroundColorNight00: { background: 'linear-gradient(180deg, rgb(39, 155, 175) 16%, rgb(66, 110, 148) 47%, rgb(30, 60, 75) 92%)' },
     }
   },
 
