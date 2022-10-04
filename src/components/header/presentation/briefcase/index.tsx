4f96bdec670198ce0a52107c29de9d8528a0c44e
cortega <cortega@acciontip.cl>
2022-10-04 13:02:32 -0500

    profile

--- src/components/header/presentation/briefcase/index.tsx
+++ src/components/header/presentation/briefcase/index.tsx
@@ -17,7 +17,7 @@ const Briefcase: FC = () => {
   /*  cntx.ptf.f_obn_stdo(std_prtf, setstd_prtf) */
 
   return <div className='divBriefcase00'>
-    <div className='divBriefcase01'></div>
+    {/* <div className='divBriefcase01'></div>
   
     <div className='divBriefcase02_00'></div>
     <div className='divBriefcase02_02'></div>
@@ -33,7 +33,7 @@ const Briefcase: FC = () => {
         <div className='divBriefcase04_00_00'></div>
       </div>
     </div>
-
+ */}
   </div>
 }
 
